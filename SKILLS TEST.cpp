27ee249f066823d39f31b56599da0fd19c7bc062
Luke <lukeeleazar.paner.eng@ust.edu.ph>
2019-03-13 12:55:12 +0800

    2

--- SKILLS TEST.cpp
+++ SKILLS TEST.cpp
@@ -5,20 +5,25 @@ using namespace std;
 
 int main()
 {
-	double inc, , pyrte, hr, ttax, net, newclth, schsupp, savb, prntadd;
+	double inc, pyrte, hr, ttax, net, newclth, schsupp, savb, prntadd;
 	inc = pyrte * (hr * 5);
 	ttax = inc * 0.14;
 	net = inc - ttax;
 	newclth = net * 0.10;
 	schsupp = net * 0.01;
-	savb = (net - newclth - schsupp) * 0.25;
+	savb = ((net - newclth) - schsupp) * 0.25;
 	prntadd = savb * 0.50;
 
 	cout << "Please input pay rate:" << endl;
 	cin >> pyrte;
 	cout << "Please input numbers of hours per week:" << endl;
 	cin >> hr;
-	cout << "Income from summer job before taxes are paid: " << inc << endl;
+	cout << "Income from summer job before taxes are paid: $" << inc << endl;
+	cout << "Income from summer job after taxes are paid: $" << net << endl;
+	cout << "The money you spend on clothes and other accessories: $" << newclth << endl;
+	cout << "The money you spend on school supplies: $" << schsupp << endl;
+	cout << "The money you spend to buy saving bonds: $ " << savb << endl;
+	cout << "The money your parents spend to buy additional savings bond for you: $ " << prntadd << endl; 
 
 	system("pause");
 	return 0;
